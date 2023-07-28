e952764d6bac2827025c0062abb162e6e29e9995
aakashvermaqss <114557019+aakashvermaqss@users.noreply.github.com>
2023-07-28 17:40:04 +0530

    chemical/company ui fixed,quotation UI completed

--- OMCIN-APP/src/app/order/order.component.ts
+++ OMCIN-APP/src/app/order/order.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { QuotationService } from '../services/quotation.service';
+
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent {
+export class OrderComponent implements OnInit {
+  subscription: Subscription | undefined;
+  quotation: any;
+  data: any[] = [];
+  searchTerm: string = '';
+
+  constructor(private quotationSerivce: QuotationService) { }
+
+  ngOnInit() {
+    this.subscription = this.quotationSerivce.getQuotation().subscribe({
+      next: (data) => {
+        this.data = data;
+      },
+      error: (error) => {
+        console.error(error);
+      }
+    });
+  }
+
+  reqDate() {
+    // Assuming the input date is stored in a variable called 'inputDate'
+    const inputDate = "2023-07-25T18:30:00.000Z";
+
+    // Convert the input date string to a JavaScript Date object
+    const dateObject = new Date(inputDate);
+
+    // Create an array to map month names
+    const monthNames = [
+      "January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November", "December"
+    ];
+
+    // Extract the day, month, and year from the date object
+    const day = dateObject.getUTCDate();
+    const month = dateObject.getUTCMonth();
+    const year = dateObject.getUTCFullYear();
+
+    // Format the date as "dd Month, yyyy"
+    const formattedDate = `${day} ${monthNames[month]}, ${year}`;
+
+    // Now 'formattedDate' contains "28 July, 2023"
+
+  }
+
+  search() {
+
+  }
+
+  filterOpenDialog() {
+
+  }
+
+  hideRow() {
+
+
+  }
+
+  deleteQuotationData(QuotationId: any) {
+
+  }
 
 }
---
+++ OMCIN-APP/src/app/services/quotation.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError } from 'rxjs/operators';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuotationService {
+
+  constructor(private http: HttpClient) { }
+
+  getQuotation(): Observable<any> {
+    const apiUrl = 'http://localhost:3000/api/quotations/getQuotations';
+
+    return this.http.get(apiUrl).pipe(
+      catchError((error: any) => {
+        return throwError(() => new Error(error));
+      })
+    );
+  }
+}
